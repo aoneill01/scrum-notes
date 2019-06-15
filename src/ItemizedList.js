4ea7e1aa39729de1b0e0c1c36a9c60161c124b8c
Andy O'Neill <aoneill01@gmail.com>
2019-06-14 20:47:02 -0400

    Add transitions for adding and removing messages

--- src/ItemizedList.js
+++ src/ItemizedList.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import ReactMarkdown from "react-markdown";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 const formatItem = item =>
   `- ${item.replace(
@@ -17,27 +18,32 @@ const ItemizedList = ({ items, onNewItem, onDeleteItem, editable }) => {
     }
   };
 
+  if (!editable) {
+    return items.map(item => <ReactMarkdown source={formatItem(item)} />);
+  }
+
   return (
     <>
-      {items.map((item, i) => (
-        <div className="row">
-          {editable && (
-            <div className="delete-row" onClick={() => onDeleteItem(i)}>
-              ×
+      <TransitionGroup>
+        {items.map((item, i) => (
+          <CSSTransition key={item} timeout={500} classNames="item">
+            <div className="row">
+              <div className="delete-row" onClick={() => onDeleteItem(i)}>
+                ×
+              </div>
+              <ReactMarkdown source={formatItem(item)} />
             </div>
-          )}
-          <ReactMarkdown source={formatItem(item)} />
-        </div>
-      ))}
-      {editable && (
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            value={newItem}
-            onChange={e => setNewItem(e.target.value)}
-          />
-        </form>
-      )}
+          </CSSTransition>
+        ))}
+      </TransitionGroup>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={newItem}
+          onChange={e => setNewItem(e.target.value)}
+        />
+      </form>
     </>
   );
 };
